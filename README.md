014c15cbc38561cdeddcc3fe857bf3e5c80de281
Jungmin, Kim <jmkim7707@gmail.com>
2024-06-11 11:08:24 +0900

    테스트: 카테고리 정보 DB입력

--- README.md
+++ README.md
@@ -1,11 +1,26 @@
 # shopper-notify
+
 shopper notify
 
+CREATE SCHEMA coupangdb;
+USE mcoupangdbydb;
 
-# Category Table
- CREATE TABLE IF NOT EXISTS CATEGORY (
-  category_id VARCHAR(255) PRIMARY KEY,
-  depth INT,
-  text VARCHAR(255),
-  parent_id INT
+# Table
+
+## Category
+
+CREATE TABLE IF NOT EXISTS CATEGORY (
+category_id VARCHAR(255) PRIMARY KEY,
+depth INT,
+text VARCHAR(255),
+parent_id VARCHAR(255)
+)
+
+## Product
+
+CREATE TABLE IF NOT EXISTS PRODUCT (
+product_id VARCHAR(255) PRIMARY KEY,
+category_id VARCHAR(255) REFERENCES CATEGORY(category_id),
+text VARCHAR(255)
+
 )
--- test/coupang_category.py
+++ test/coupang_category.py
@@ -1,8 +1,9 @@
 ####################################
-# 설치
-# % pip install selenium BeautifulSoup4
+# 패키지 설치
 ####################################
-
+# conda create -n crawling python=3.11
+# conda activate crawling
+# pip install selenium BeautifulSoup4 mysql-connector-python
 
 ####################################
 # 모듈 import
@@ -61,68 +62,114 @@ html_data = driver.page_source
 # BeautifulSoup 객체 생성
 soup = BeautifulSoup(html_data, 'html.parser')
 
+
+
+insertDataList = [] # DB 입력될 데이터 리스트
+
 # '#gnbAnalytics > ul.menu.shopping-menu-list' 선택자로 요소 찾기
 menu_list = soup.select_one('#gnbAnalytics > ul.menu.shopping-menu-list')
 
 if menu_list:
     # 모든 <li> 요소 찾기
-    li_elements = menu_list.find_all('li', recursive=False) # recursive=False 면 바로 밑 하위 엘레먼트만 조회
-
-    for li in li_elements:
+    depth1_li_el_list = menu_list.find_all('li', recursive=False) # recursive=False 면 바로 밑 하위 엘레먼트만 조회
+    for depth1_li_el in depth1_li_el_list:
         # depth1 텍스트 추출 (li 바로 아래의 첫 번째 <a> 태그)
-        depth1_text = li.find('a', recursive=False)
-        if depth1_text:
-            print("Depth 1:", depth1_text.get_text(strip=True))
+        a_el = depth1_li_el.find('a', recursive=False)
+        if a_el:
+            a_href_el = a_el.get('href')
+
+            depth1_category_id = 'None'
+            if len(a_href_el.split('/')) > 3:                
+                depth1_category_id = a_href_el.split('/')[3]            
+
+            text = a_el.get_text(strip=True) #카테고리명
+
+            insertDataList.append({
+                'category_id': depth1_category_id,
+                'depth': 1,
+                'text': text,
+                'parent_id': None
+            })
+
+            print("Depth 1:", text, " categoryid=", depth1_category_id)
 
             # depth2 텍스트 추출
-            depth2_elements = li.find_all('li', class_='second-depth-list')
-            for depth2 in depth2_elements:
-                depth2_text = depth2.find('a')
-                if depth2_text:
-                    print("  Depth 2:", depth2_text.get_text(strip=True))
+            depth2_li_el_list = depth1_li_el.find_all('li', class_='second-depth-list')
+            for depth2_li_el in depth2_li_el_list:
+                a_el = depth2_li_el.find('a')
+                if a_el:
+                    a_href_el = a_el.get('href')
+
+                    depth2_category_id = 'None'
+                    if len(a_href_el.split('/')) > 3:                
+                        depth2_category_id = a_href_el.split('/')[3]            
+                    text = a_el.get_text(strip=True) #카테고리명
+
+                    insertDataList.append({
+                        'category_id': depth2_category_id,
+                        'depth': 2,
+                        'text': text,
+                        'parent_id': depth1_category_id
+                    })
+
+                    print("  Depth 2:", text, " categoryid=", depth2_category_id)
 
                     # depth3 텍스트 추출
-                    depth3_elements = depth2.find_all('li')
-                    for depth3 in depth3_elements:
-                        depth3_text = depth3.find('a')
-                        if depth3_text:
-                            print("    Depth 3:", depth3_text.get_text(strip=True))
+                    depth3_li_el_list = depth2_li_el.find_all('li')
+                    for depth3_li_el in depth3_li_el_list:
+                        a_el = depth3_li_el.find('a')
+                        if a_el:                            
+                            a_href_el = a_el.get('href')
+
+                            depth3_category_id = 'None'
+                            if len(a_href_el.split('/')) > 3:                
+                                depth3_category_id = a_href_el.split('/')[3]
+                            text = a_el.get_text(strip=True) #카테고리명
+
+                            insertDataList.append({
+                                'category_id': depth3_category_id,
+                                'depth': 3,
+                                'text': text,
+                                'parent_id': depth2_category_id
+                            })
+                            print("    Depth 3:", text, " categoryid=", depth2_category_id)
 
             print()  # 각 depth1 요소 사이에 빈 줄 추가
 else:
     print("요소를 찾을 수 없습니다.")
     
-    
+
 # 출력을 원래대로 되돌리고 파일 닫기
 sys.stdout.close()
 sys.stdout = original_stdout
 print("출력이 'xs.txt' 파일에 저장되었습니다.")
 
-
-
-import mysql.connector
-
 ####################################
 # MySQL 데이터베이스에 연결
 ####################################
+import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
-  user="yourusername",
-  password="yourpassword",
-  database="mydatabase"
+  user="coupang",
+  password="coupang!",
+  database="coupangdb"
 )
 
 # 커서 객체 생성
 mycursor = mydb.cursor()
 
+
+####################################
+# List 데이터 입력
+####################################
+
 # 데이터를 삽입할 SQL 쿼리
-sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-val = ("John", "Highway 21")
+sql = "INSERT INTO category (category_id, depth, text, parent_id) VALUES (%s, %s, %s, %s)"
 
-# 쿼리 실행
-mycursor.execute(sql, val)
+for data in insertDataList:
+    values = (data['category_id'], data['depth'], data['text'], data['parent_id'])
+    mycursor.execute(sql, values)
 
-# 변경사항 커밋 (중요!)
 mydb.commit()
 
 print(mycursor.rowcount, "record inserted.")
